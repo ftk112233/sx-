dde09cba434cf5fc74e6c689ee4514c5f50fecf1
ftk <1056907544@qq.com>
2019-12-12 17:01:21 +0800

    第san次提交测试

--- src/main/java/com/mt/sx/pojo/vo/SxProductVO.java
+++ src/main/java/com/mt/sx/pojo/vo/SxProductVO.java
@@ -6,7 +6,7 @@ public class SxProductVO extends SxProduct {
     private Integer page;
     private Integer pageSize;
 
-    public Integer getPage() {
+   /* public Integer getPage() {
         return page;
     }
 
@@ -20,5 +20,5 @@ public class SxProductVO extends SxProduct {
 
     public void setPageSize(Integer pageSize) {
         this.pageSize = pageSize;
-    }
+    }*/
 }
