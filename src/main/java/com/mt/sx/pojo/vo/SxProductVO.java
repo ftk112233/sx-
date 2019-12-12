d9d939c0cbc50d232ace01c8b75142ac52c360e1
baifengliu <734055580@qq.com>
2019-12-12 16:52:48 +0800

    第二次提交

---
+++ src/main/java/com/mt/sx/pojo/vo/SxProductVO.java
@@ -0,0 +1,24 @@
+package com.mt.sx.pojo.vo;
+
+import com.mt.sx.pojo.SxProduct;
+
+public class SxProductVO extends SxProduct {
+    private Integer page;
+    private Integer pageSize;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
