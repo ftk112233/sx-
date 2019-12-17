ec945e7ce67ce5249a4f500d70b7fd26bec6ed56
ftk <1056907544@qq.com>
2019-12-17 15:33:15 +0800

    13日下午四点半提交测试

--- src/main/java/com/mt/sx/mapper/SxUserMapper.java
+++ src/main/java/com/mt/sx/mapper/SxUserMapper.java
@@ -2,7 +2,6 @@ package com.mt.sx.mapper;
 
 import com.mt.sx.common.config.MyMapper;
 import com.mt.sx.pojo.SxUser;
-import tk.mybatis.mapper.additional.idlist.IdListMapper;
 
-public interface SxUserMapper extends MyMapper<SxUser> , IdListMapper<SxUser,Integer> {
+public interface SxUserMapper extends MyMapper<SxUser> {
 }
