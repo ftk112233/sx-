2c9a46f14bcb22c6931fa924f6a13e38d85b3d7b
ftk <1056907544@qq.com>
2019-12-27 10:59:53 +0800

    27日上午设施

--- src/main/java/com/mt/sx/common/exception/GlobalException.java
+++ src/main/java/com/mt/sx/common/exception/GlobalException.java
@@ -1,22 +1,15 @@
 package com.mt.sx.common.exception;
 
 
-import com.mt.sx.common.enums.ResponseCode;
-
 /**
  * 自定义全局异常
  */
 public class GlobalException extends RuntimeException {
 
-    public GlobalException( int code,String message) {
+    public GlobalException(String message, int code) {
         super(message);
         this.code = code;
     }
-    public GlobalException(ResponseCode responseCode) {
-        super(responseCode.getMsg());
-        this.code = responseCode.getCode();
-    }
-
 
     private int code;
 
