3f37668b46aa7c63964bd079d9f78ee4670570cc
ftk <1056907544@qq.com>
2019-12-27 13:40:12 +0800

    27日提交swagger过滤器

--- src/main/java/com/mt/sx/common/shiro/ShiroAutoConfiguration.java
+++ src/main/java/com/mt/sx/common/shiro/ShiroAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.mt.sx.common.shiro;
 
+import com.mt.sx.controller.LoginController;
 import lombok.Data;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
@@ -13,6 +14,7 @@ import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreato
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -87,15 +89,16 @@ public class ShiroAutoConfiguration {
 
         filterChainDefinitionMap.put("/login", "anon");
         filterChainDefinitionMap.put("/user/insert", "anon");
+        filterChainDefinitionMap.put("/swagger-ui.html", "anon");
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/v2/**", "anon");
+        filterChainDefinitionMap.put("/swagger-resources/**", "anon");
 
         // 设置登出的路径
 
         filterChainDefinitionMap.put("/logout", "logout");
 
         // 设置拦截的路径
-
-        filterChainDefinitionMap.put("/**", "jwt");
-
 
         factoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return factoryBean;
